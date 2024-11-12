e121494de78bd5f36dba747b7fa2fc9b7801f0c6
JSayWhat <million.jason@gmail.com>
2024-11-12 09:27:09 -0500

    Implement player shooting mechanics and asteroid collision handling

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -14,6 +18,13 @@ def main():
 
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
+
+  Asteroid.containers = (updatable, drawable, asteroids)
+  Shot.containers = (shots, updatable, drawable)
+  AsteroidField.containers = updatable
+  asteroid_field = AsteroidField()
   
   Player.containers = (updatable, drawable)
   
@@ -28,7 +39,17 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
-        
+
+        for asteroid in asteroids:
+            if asteroid.collisions(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collisions(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
         
         for obj in drawable:
@@ -39,6 +60,5 @@ def main():
         dt = clock.tick(60) / 1000
 
   
-
 if __name__ == "__main__":
     main()
